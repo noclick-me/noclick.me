4b98555a7f57d2129b71cd9c3c075f5d3c541aa7
Leandro Lucarella <luca@llucax.com>
2020-12-22 15:37:21 +0100

    Update repo, pub package and app name
    The repository was renamed to noclick.me, the pub package was renamed to
    noclick_me and the app was renamed to noclick.me.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:noclick_me/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
