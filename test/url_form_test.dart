d725b21f0082d5676a7c4e787771b0f9736639d7
Leandro Lucarella <luca@llucax.com>
2020-12-24 20:39:41 +0100

    Add UrlForm tests for success submit cases

--- test/url_form_test.dart
+++ test/url_form_test.dart
@@ -1,27 +1,32 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
-import 'package:noclick_me/screenutil_builder.dart';
+import 'package:flutter/material.dart' hide HttpClientProvider;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' show Request, Response;
+import 'package:http/testing.dart' show MockClient, MockClientHandler;
+
+import 'package:noclick_me/provider/http_client_provider.dart'
+    show HttpClientProvider;
+import 'package:noclick_me/screenutil_builder.dart' show screenutilBuilder;
 import 'package:noclick_me/url_form.dart';
 
 void main() {
   group('UrlForm', () {
     const errorMsg = 'Please enter a valid http/https internet URL';
 
-    Widget urlForm;
-
-    setUp(() {
-      urlForm = MaterialApp(
-        home: Material(
-          child: screenutilBuilder(
-            child: UrlForm(),
+    Widget createForm({MockClientHandler mockClientHandler}) => MaterialApp(
+          home: Scaffold(
+            body: HttpClientProvider(
+              client: MockClient(mockClientHandler ?? (r) => null),
+              child: screenutilBuilder(
+                child: UrlForm(),
+              ),
+            ),
           ),
-        ),
-      );
-    });
+        );
 
     testWidgets('have all important widgets', (WidgetTester tester) async {
-      await tester.pumpWidget(urlForm);
+      await tester.pumpWidget(createForm());
       final expandButtonFinder = find.byType(FlatButton);
       final urlFieldFinder = find.byType(TextFormField);
 
@@ -32,10 +37,10 @@ void main() {
       expect(find.text(errorMsg), findsNothing);
     });
 
-    group('gets an error when', () {
+    group('errors when', () {
       testWidgets('tapping on button without input',
           (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final expandButton = find.byType(FlatButton);
 
         await tester.tap(expandButton);
@@ -45,7 +50,7 @@ void main() {
       });
 
       testWidgets('pressing ENTER on empty field', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
 
         await tester.testTextInput.receiveAction(TextInputAction.done);
         await tester.pumpAndSettle();
@@ -54,7 +59,7 @@ void main() {
       });
 
       testWidgets('the URL is a word', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final expandButton = find.byType(FlatButton);
         final urlField = find.byType(TextFormField);
 
@@ -78,7 +83,7 @@ void main() {
       });
 
       testWidgets('the URL starts with .', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final urlField = find.byType(TextFormField);
 
         await tester.enterText(urlField, '.invalid');
@@ -87,7 +92,7 @@ void main() {
       });
 
       testWidgets('the URL ends with .', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final urlField = find.byType(TextFormField);
 
         await tester.enterText(urlField, 'invalid.');
@@ -97,7 +102,7 @@ void main() {
 
       testWidgets('the URL is an invalid URI with schema',
           (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final urlField = find.byType(TextFormField);
 
         await tester.enterText(urlField, 'http!://invalid*');
@@ -106,7 +111,7 @@ void main() {
       });
 
       testWidgets('the URL is a path (no domain)', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final urlField = find.byType(TextFormField);
 
         await tester.enterText(urlField, 'invalid/bad/url');
@@ -115,13 +120,88 @@ void main() {
       });
 
       testWidgets('the URL schema is not http(s)', (WidgetTester tester) async {
-        await tester.pumpWidget(urlForm);
+        await tester.pumpWidget(createForm());
         final urlField = find.byType(TextFormField);
 
         await tester.enterText(urlField, 'ftp://invalid.com/bad/url');
         await tester.pumpAndSettle();
         expect(find.text('Only http/https URLs are supported'), findsOneWidget);
       });
+
+      testWidgets('server response is not 200 OK', (WidgetTester tester) async {
+        Future<Response> createUrlHandler(Request r) async {
+          final dynamic req = jsonDecode(r.body);
+          final u = Uri.parse(req['url'].toString());
+          final path = '${u.scheme}/${u.host}${u.path}/long';
+          return Response('{"noclick_url": "https://noclick.me/$path"}', 200);
+        }
+
+        await tester.pumpWidget(createForm(
+          mockClientHandler: (r) async => Response('BAD', 400),
+        ));
+
+        await tester.enterText(
+          find.byType(TextFormField),
+          'https://example.com',
+        );
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pump(Duration(seconds: 6));
+        expect(find.byType(SnackBar), findsOneWidget);
+        expect(find.text('Could not retrieve the page'), findsOneWidget);
+      }, skip: true); // FIXME: I CAN'T FIND THE SNACKBAR with the error
+    });
+
+    group('succeeds', () {
+      void testInputSucceeds(
+          {@required String input, @required String output}) {
+        Future<Response> createUrlHandler(Request r) async {
+          final dynamic req = jsonDecode(r.body);
+          final u = Uri.parse(req['url'].toString());
+          final path = '${u.scheme}/${u.host}${u.path}/long';
+          return Response('{"noclick_url": "https://noclick.me/$path"}', 200);
+        }
+
+        testWidgets('for $input', (WidgetTester tester) async {
+          await tester.pumpWidget(createForm(
+            mockClientHandler: createUrlHandler,
+          ));
+
+          await tester.enterText(find.byType(TextFormField), input);
+          await tester.testTextInput.receiveAction(TextInputAction.done);
+          await tester.pumpAndSettle();
+          expect(find.text(output), findsOneWidget);
+        });
+      }
+
+      testInputSucceeds(
+        input: 'example.com',
+        output: 'https://noclick.me/https/example.com/long',
+      );
+
+      testInputSucceeds(
+        input: '/example.com',
+        output: 'https://noclick.me/https/example.com/long',
+      );
+
+      testInputSucceeds(
+        input: '//example.com',
+        output: 'https://noclick.me/https/example.com/long',
+      );
+
+      testInputSucceeds(
+        input: 'example.com/with/path',
+        output: 'https://noclick.me/https/example.com/with/path/long',
+      );
+
+      testInputSucceeds(
+        input: '/example.com/with/path',
+        output: 'https://noclick.me/https/example.com/with/path/long',
+      );
+
+      testInputSucceeds(
+        input: '//example.com/with/path',
+        output: 'https://noclick.me/https/example.com/with/path/long',
+      );
     });
   });
 }
